66d4bfaeb9fe5c1c3a182ce23b689644f24339e0
Михаил Эйсвальд <e_misha_vl@mail.ru>
2023-04-18 16:31:47 +0300

    no dependencies synchronous stub

---
+++ synchronous/src/main/java/ru/etu/stud/java/synchronous/Application.java
@@ -0,0 +1,12 @@
+package ru.etu.stud.java.synchronous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Hello World!");
+    }
+}
