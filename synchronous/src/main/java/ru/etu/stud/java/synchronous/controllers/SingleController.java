1e2745ff6a3796b369b7db53e9d5156795e835f5
Михаил Эйсвальд <e_misha_vl@mail.ru>
2023-04-23 15:14:35 +0300

    synchronous: measure blocked time with ThreadMXBean

--- synchronous/src/main/java/ru/etu/stud/java/synchronous/controllers/SingleController.java
+++ synchronous/src/main/java/ru/etu/stud/java/synchronous/controllers/SingleController.java
@@ -1,23 +1,35 @@
 package ru.etu.stud.java.synchronous.controllers;
 
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Gauge;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import ru.etu.stud.java.synchronous.tools.ThreadObserver;
 import ru.etu.stud.java.synchronous.domain.DatabaseEntity;
 import ru.etu.stud.java.synchronous.services.SingleService;
 
 @RestController
 public class SingleController {
     private final SingleService singleService;
-    public  SingleController(SingleService singleService) {
+    private final ThreadObserver threadObserver;
+    private final Gauge averageBlockTime;
+    private final Logger logger = LoggerFactory.getLogger(ThreadObserver.class);
+    public SingleController(SingleService singleService, ThreadObserver threadObserver, Gauge averageBlockTime) {
         this.singleService = singleService;
+        this.threadObserver = threadObserver;
+        this.averageBlockTime = averageBlockTime;
     }
     @Timed(percentiles = {0.5, 0.75, 0.95, 0.99}, value = "synchronous.controller.one")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public DatabaseEntity getOneByApiId(@PathVariable long id) {
-        return singleService.getData(id);
+        DatabaseEntity response = this.singleService.getData(id);
+        this.threadObserver.registerBlockedTime(Thread.currentThread());
+        this.averageBlockTime.measure();
+        return response;
     }
     @Timed(percentiles = {0.5, 0.75, 0.95, 0.99}, value = "synchronous.controller.all")
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
