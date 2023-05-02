2638a2fdd0d23dce20d1786594b30cccf17cc394
Михаил Эйсвальд <e_misha_vl@mail.ru>
2023-05-02 15:50:37 +0300

    reactive: time before first publish

--- reactive/src/main/java/ru/etu/stud/java/reactive/controllers/SingleController.java
+++ reactive/src/main/java/ru/etu/stud/java/reactive/controllers/SingleController.java
@@ -1,5 +1,6 @@
 package ru.etu.stud.java.reactive.controllers;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +16,12 @@ public class SingleController {
     public  SingleController(SingleService singleService) {
         this.singleService = singleService;
     }
+    @Timed(percentiles = {0.5, 0.75, 0.95, 0.99}, value = "reactive.controller.one")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Mono<DatabaseEntity> getOneByApiId(@PathVariable long id) {
         return singleService.getData(id);
     }
+    @Timed(percentiles = {0.5, 0.75, 0.95, 0.99}, value = "reactive.controller.all")
     @GetMapping(value = "/", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<DatabaseEntity> getAll() {
         return singleService.getData();
--- reactive/src/main/java/ru/etu/stud/java/reactive/services/SingleService.java
+++ reactive/src/main/java/ru/etu/stud/java/reactive/services/SingleService.java
@@ -1,21 +1,43 @@
 package ru.etu.stud.java.reactive.services;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.etu.stud.java.reactive.domain.DatabaseEntity;
 import ru.etu.stud.java.reactive.repositories.SingleRepository;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class SingleService {
     private final SingleRepository repository;
-    public SingleService(SingleRepository repository) {
+    private final MeterRegistry registry;
+    public SingleService(SingleRepository repository, MeterRegistry registry) {
         this.repository = repository;
+        this.registry = registry;
+        Timer.builder("reactive.first.publish.one").register(registry);
+        Timer.builder("reactive.first.publish.all").register(registry);
     }
     public Mono<DatabaseEntity> getData(long id) {
-        return repository.findById(id);
+        Mono<DatabaseEntity> response = repository.findById(id);
+        response = response.elapsed().flatMap(tuple -> {
+            registry.timer("reactive.first.publish.one")
+                    .record(tuple.getT1(), TimeUnit.MILLISECONDS);
+            return Mono.just(tuple.getT2());
+        });
+        return response;
     }
     public Flux<DatabaseEntity> getData() {
-        return repository.findAll();
+        Flux<DatabaseEntity> response = repository.findAll();
+        response = response.elapsed().switchOnFirst(
+                (signal, flux) -> {
+                    registry.timer("reactive.first.publish.all")
+                            .record(signal.get().getT1(), TimeUnit.MILLISECONDS);
+                    return flux;
+                }
+        ).flatMap(tuple -> Mono.just(tuple.getT2()));
+        return response;
     }
 }
