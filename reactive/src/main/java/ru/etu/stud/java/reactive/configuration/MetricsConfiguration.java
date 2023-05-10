98aeb3055559d3c2a2aa323199c828e323b27a52
Михаил Эйсвальд <e_misha_vl@mail.ru>
2023-05-10 22:18:30 +0300

    reactive: fix timers

--- reactive/src/main/java/ru/etu/stud/java/reactive/configuration/MetricsConfiguration.java
+++ reactive/src/main/java/ru/etu/stud/java/reactive/configuration/MetricsConfiguration.java
@@ -2,6 +2,11 @@ package ru.etu.stud.java.reactive.configuration;
 
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.config.MeterRegistryConfig;
+import io.micrometer.core.instrument.config.validate.Validated;
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- reactive/src/main/java/ru/etu/stud/java/reactive/controllers/SingleController.java
+++ reactive/src/main/java/ru/etu/stud/java/reactive/controllers/SingleController.java
@@ -1,29 +1,57 @@
 package ru.etu.stud.java.reactive.controllers;
 
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.config.MeterRegistryConfig;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
 import ru.etu.stud.java.reactive.domain.DatabaseEntity;
 import ru.etu.stud.java.reactive.services.SingleService;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class SingleController {
     private final SingleService singleService;
-    public  SingleController(SingleService singleService) {
+    private final MeterRegistry meterRegistry;
+    public  SingleController(SingleService singleService, MeterRegistry meterRegistry) {
         this.singleService = singleService;
+        this.meterRegistry = meterRegistry;
+        Timer.builder("reactive.controller.one").register(meterRegistry);
+        Timer.builder("reactive.controller.all").register(meterRegistry);
     }
-    @Timed(percentiles = {0.5, 0.75, 0.95, 0.99}, value = "reactive.controller.one")
+    //@Timed(percentiles = {0.5, 0.75, 0.95, 0.99}, value = "reactive.controller.one")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Mono<DatabaseEntity> getOneByApiId(@PathVariable long id) {
-        return singleService.getData(id);
+        long startTime = System.currentTimeMillis();
+        Mono<DatabaseEntity> response = singleService.getData(id);
+        response = response.doFinally(signalType -> {
+            if(signalType == SignalType.ON_COMPLETE) {
+                meterRegistry.timer("reactive.controller.one").record(
+                        System.currentTimeMillis() - startTime, TimeUnit.MILLISECONDS
+                );
+            }
+        });
+        return response;
     }
-    @Timed(percentiles = {0.5, 0.75, 0.95, 0.99}, value = "reactive.controller.all")
+    //@Timed(percentiles = {0.5, 0.75, 0.95, 0.99}, value = "reactive.controller.all")
     @GetMapping(value = "/", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<DatabaseEntity> getAll() {
-        return singleService.getData();
+        long startTime = System.currentTimeMillis();
+        Flux<DatabaseEntity> response = singleService.getData();
+        response = response.doFinally(signalType -> {
+            if(signalType == SignalType.ON_COMPLETE) {
+                meterRegistry.timer("reactive.controller.all").record(
+                        System.currentTimeMillis() - startTime, TimeUnit.MILLISECONDS
+                );
+            }
+        });
+        return response;
     }
 }
